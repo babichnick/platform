0b8f92a4fa809914ca7a15f0b2951b719a31cb13
Nick Babich <nick@babich.biz>
2018-11-08 00:32:43 +0300

    normal url

--- ux/urls.py
+++ ux/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from prototyping import views
-from machina.app import board
 
 
 urlpatterns = [
@@ -36,7 +35,6 @@ urlpatterns = [
     #path('blog/page<int:num>/', views.blog),
     path('about/', views.about),
     path('contact/' , views.contact_me),
-    path('community/', include(board.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
